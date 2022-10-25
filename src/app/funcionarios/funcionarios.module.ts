ed20f6f38c95d169e868a6cdbc901db6085186e6
bragavaas <bragavaas@gmail.com>
2022-10-25 02:54:28 -0300

    Adicionando mais arquivos de interface e de services

--- src/app/funcionarios/funcionarios.module.ts
+++ src/app/funcionarios/funcionarios.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 import { FuncionariosRoutingModule } from './funcionarios-routing.module';
 import { FuncionariosComponent } from './funcionarios/funcionarios.component';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +13,7 @@ import { FuncionariosComponent } from './funcionarios/funcionarios.component';
   imports: [
     CommonModule,
     FuncionariosRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule
+    AppMaterialModule
   ]
 })
 export class FuncionariosModule { }
