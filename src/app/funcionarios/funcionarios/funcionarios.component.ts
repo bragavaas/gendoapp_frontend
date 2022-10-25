7f9608aefec3b6b23f20a00921b72d779b308604
bragavaas <bragavaas@gmail.com>
2022-10-25 04:45:09 -0300

    trabalhando no servico de funcionario

--- src/app/funcionarios/funcionarios/funcionarios.component.ts
+++ src/app/funcionarios/funcionarios/funcionarios.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
 import { Funcionario } from '../model/funcionario';
+import { FuncionariosService } from '../services/funcionarios.service';
 
 @Component({
   selector: 'app-funcionarios',
@@ -8,19 +12,16 @@ import { Funcionario } from '../model/funcionario';
 })
 export class FuncionariosComponent implements OnInit {
 
-  funcionarios: Funcionario[] = [
-    {_id:"1", nome: "André", celular: "21997235420", email: "bragavaas@gmail.com", cpf: "15447565774" , expediente: "SEG A SEX: 8 AS 17h", observacoes: "N/A"},
-    {_id:"2", nome: "Lucas", celular: "219972456123", email: "lpiazzi@gmail.com", cpf: "1627482959" , expediente: "SEG A SEX: 8 AS 17h", observacoes: "N/A"},
-    {_id:"3", nome: "Tiago", celular: "21983644255", email: "t.carvalho@gmail.com", cpf: "2951723658" , expediente: "SEG A SEX: 8 AS 17h", observacoes: "N/A"},
-    {_id:"4", nome: "Denise", celular: "2199867524", email: "dgbrag@gmail.com", cpf: "12745925628" , expediente: "SEG A SEX: 8 AS 17h", observacoes: "N/A"},
-    {_id:"5", nome: "Gleisson", celular: "2199327721", email: "gleisson@gmail.com", cpf: "12724824628" , expediente: "SEG A SEX: 8 AS 17h", observacoes: "N/A"},
-    {_id:"6", nome: "Roberto", celular: "2197442532", email: "r.alves@gmail.com", cpf: "12828462728" , expediente: "SEG A SEX: 8 AS 17h", observacoes: "N/A"}
-  ];
+  funcionarios: Observable<Funcionario[]>;
   displayedColumns = ['nome', 'expediente', 'observacoes'];
-  constructor() {
+
+  constructor(private funcionariosService: FuncionariosService ) {
+    this.funcionarios = this.funcionariosService.list();
   }
+  
 
   ngOnInit(): void {
   }
 
 }
+
--- src/app/funcionarios/services/funcionarios.service.ts
+++ src/app/funcionarios/services/funcionarios.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Funcionario } from '../model/funcionario';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/internal/operators/tap';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionariosService {
 
-  constructor() { }
+  private readonly API = '/assets/funcionarios.json';
+  constructor(private httpClient: HttpClient ) {  }
+
+  list() {
+   return this.httpClient.get<Funcionario[]>(this.API)
+   .pipe(
+    tap((funcionarios: any) => console.log(funcionarios))
+   );
+  }
 }
